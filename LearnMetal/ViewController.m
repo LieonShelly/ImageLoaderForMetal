0ca9a729fdc3921d4f0e43fd1cac64a17586952a
renjun <lieon@gmail.com>
2022-05-31 00:11:12 +0800

    update

--- LearnMetal/ViewController.m
+++ LearnMetal/ViewController.m
@@ -3,7 +3,7 @@
 //  LearnMetal
 //
 //  Created by loyinglin on 2018/6/21.
-//  Copyright © 2018年 loyinglin. All rights reserved.
+
 //
 @import MetalKit;
 #import "LYShaderTypes.h"
--- LearnMetal/shaders.metal
+++ LearnMetal/shaders.metal
@@ -3,7 +3,7 @@
 //  LearnMetal
 //
 //  Created by loyinglin on 2018/6/21.
-//  Copyright © 2018年 loyinglin. All rights reserved.
+
 //
 
 #include <metal_stdlib>
